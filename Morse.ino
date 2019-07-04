2e281d5faf88d26c27c9a041d0a22184db8b9c92
xidianzhy <52474929+xidianzhy@users.noreply.github.com>
2019-07-04 13:01:43 +0800

    Update Morse.ino

--- Morse.ino
+++ Morse.ino
@@ -16,17 +16,17 @@ void setup() {
 void loop() {
   String str = "";    //储存串口读到的字符串
   String output = "";  //转义后字符串
-  int i, t , temp = 0;
-  int n = 0;  //对传入字符个数计数
+  int i, t , q = 0;
+  int n = 0;  //传入字符个数
   while (Serial.available() > 0) {
-    temp = 1;  //判断是否有数据传入
+    q = 1;  //判断是否有数据传入
     str += char(Serial.read());  //将传入的数据储存在字符串中
-    delay(1000);  //延迟保证传入正常
+    delay(1000);  //延迟1s
     n++;
   }
 
-  if (temp) {
-    //查询Morse电码表并进行转换
+  if (q) {
+    //查询Morse表进行转换
     for (i = 0; i < n; i++)
     {
 
@@ -36,10 +36,8 @@ void loop() {
           output += String(MORSE[int(str[i] - 97)]);
         }
         else output += String(MORSE[int(str[i] - 65)]);
-
-      
     }
-    Serial.print(output);  //串口传入
+    Serial.print(output);  //打印在串口监视器
   }
     for(int j=0;j<output.length()-1;j++)
     {
@@ -51,11 +49,11 @@ void loop() {
         {
          morse.dash();
        }
-       if(output[j]==32)
+       if(output[j]==32)//如果空格，延迟3s
        {
         delay(3000);
        }
-       if(output[j]==13)
+       if(output[j]==13)//如果回车，延迟3s
        {
         delay(3000);
         }
