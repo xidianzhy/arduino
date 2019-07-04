75d155501b79d06d088ed8b2750ef2a956ef2fb4
xidianzhy <52474929+xidianzhy@users.noreply.github.com>
2019-07-04 10:01:40 +0800

    Add files via upload

---
+++ sizzling_hillar1.ino
@@ -0,0 +1,98 @@
+void setup()
+{
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(13, OUTPUT);
+  pinMode(4,  OUTPUT);
+  Serial.begin(9600);
+}
+int income=0;
+void loop()
+{
+  if(Serial.available()>0)
+    {
+      income=Serial.read();
+        switch(income)
+        {
+          case 'f':
+              forward();
+              break;
+            case 'b':
+              backward();
+              break;
+            case 'l':
+              left();
+              break;
+            case 'r':
+              right();
+              break;
+            case 's':
+              stop();
+              break;
+            default:
+              break;
+        }
+    }
+}
+
+void forward()
+{
+  digitalWrite(5,HIGH);
+  digitalWrite(6,LOW);
+  digitalWrite(9,HIGH);
+  digitalWrite(10,LOW);
+}
+
+void backward()
+{
+  digitalWrite(6,HIGH);
+  digitalWrite(5,LOW);
+  digitalWrite(10,HIGH);
+  digitalWrite(9,LOW);
+}
+
+void left()
+{
+  digitalWrite(5,HIGH);
+  digitalWrite(6,LOW);
+  digitalWrite(10,HIGH);
+  digitalWrite(9,LOW);
+  while(income=='l')
+  {
+   digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW);
+  delay(1000);
+  }
+  while(income!='l') digitalWrite(13,LOW);
+  
+}
+
+void right()
+{
+  digitalWrite(6,HIGH);
+  digitalWrite(5,LOW);
+  digitalWrite(9,HIGH);
+  digitalWrite(10,LOW);
+  while(income=='r')
+  {
+    digitalWrite(4,HIGH);
+     delay(1000);
+  digitalWrite(4,LOW);
+  delay(1000);
+  }
+  while(income!='r')
+    digitalWrite(4,LOW);
+   
+  
+}
+
+void stop()
+{
+  digitalWrite(5,LOW);
+  digitalWrite(6,LOW);
+  digitalWrite(9,LOW);
+  digitalWrite(10,LOW);
+}
